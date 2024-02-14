ee354da9cf19047584887198df2c480e5153239f
Anton Glotov <vlastelin.colec.com@mail.ru>
2024-02-14 13:16:58 +0300

    Функция возвращает количество русских символов в строке
    Signed-off-by: Anton Glotov <vlastelin.colec.com@mail.ru>

--- task6-8.py
+++ task6-8.py
@@ -3,3 +3,11 @@ def is_russian(symb):
     if russian_code[0] < ord(symb) < russian_code[1]:
         return True
     return False
+
+def count_russian(sentence):
+    count = 0
+    for i in sentence:
+        if is_russian(i):
+            count += 1
+    return count
+
