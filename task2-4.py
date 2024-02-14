06221e38abfaa49d05f3d992d93a7c48f07b5678
Anton Glotov <vlastelin.colec.com@mail.ru>
2024-02-14 03:46:19 +0300

    Функция подсчета количества слов в предложении с четным количеством символов
    Signed-off-by: Anton Glotov <vlastelin.colec.com@mail.ru>

--- task2-4.py
+++ task2-4.py
@@ -1,4 +1,5 @@
 import random
+sentence = "раз два три четыре пять шесть семь восемь"
 
 
 def shuffle(s1):
@@ -6,3 +7,12 @@ def shuffle(s1):
     random.shuffle(words)
     s1 = ' '.join(words)
     return s1
+
+
+def count_even_words(s2):
+    words = s2.split()
+    count = 0
+    for i in words:
+        if len(i) % 2 == 0:
+            count += 1
+    return count
