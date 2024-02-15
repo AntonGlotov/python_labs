64aa7de735295fc9c9561c8ef8b77080b88c6db4
Anton Glotov <vlastelin.colec.com@mail.ru>
2024-02-15 07:52:59 +0300

    Функция осуществляет циклический сдвиг элементов массива влево на одну позицию.
    Signed-off-by: Anton Glotov <vlastelin.colec.com@mail.ru>

--- task15-19.py
+++ task15-19.py
@@ -11,3 +11,9 @@ def loc_min(massive, index):
     return False
 
 
+def shift_mass(massive):
+    temp = massive[0]
+    for i in range(0, len(massive) - 1):
+        massive[i] = massive[i+1]
+    massive[len(massive) - 1] = temp
+    return massive
