25c19757d388651f6602e93bcf0d5027704f732a
Anton Glotov <vlastelin.colec.com@mail.ru>
2024-02-15 09:30:51 +0300

    Готовая программа
    Signed-off-by: Anton Glotov <vlastelin.colec.com@mail.ru>

--- task15-19.py
+++ task15-19.py
@@ -38,3 +38,33 @@ def oper_with_list(list):
         L2.append(list.count(i))
     return L1, L2
 
+
+print("1 - Проверить, является ли i-ый элемент глобальным максимумом списка\n"
+      "2 - Проверить, является ли i-ый элемент списка локальным минимумом\n"
+      "3 - Осуществить циклический сдвиг элементов массива влево\n"
+      "4 - Вывести сначала четные потом нечетные элементы массива\n"
+      "5 - Построить два списка, в первом будут неповторяющиеся элементы исходного списка,\n"
+      "    а во втором будет количество, сколько раз элемент первого встретился в исходном\n"
+      "Выберите, чем хотите заняться:")
+
+number = int(input())
+
+print("Введите список: ")
+mass = input()
+mass = mass.split()
+mass = [int(i) for i in mass]
+
+if number == 1:
+    print("Введите номер элемента")
+    print(glob_max(mass, int(input())))
+elif number == 2:
+    print("Введите номер элемента")
+    print(loc_min(mass, int(input())))
+elif number == 3:
+    print(shift_mass(mass))
+elif number == 4:
+    print_mass_ch_nch(mass)
+elif number == 5:
+    l1, l2 = oper_with_list(mass)
+    print(l1)
+    print(l2)
