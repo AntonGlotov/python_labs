bf0c5efdb25a3a26239f5a3a3a7d1a057e9e0fe4
Anton Glotov <vlastelin.colec.com@mail.ru>
2024-03-07 00:30:52 +0300

    class Rectangle
    Signed-off-by: Anton Glotov <vlastelin.colec.com@mail.ru>

--- Lab3/Classes.py
+++ Lab3/Classes.py
@@ -3,7 +3,7 @@ import turtle
 
 
 class Triangle:
-    def __init__(self, coord_x=0, coord_y=0, size=100, color="red"):
+    def __init__(self, coord_x=0, coord_y=0, size=100, color="blue"):
         self.coord_x = coord_x
         self.coord_y = coord_y
         self.size = size
@@ -28,4 +28,30 @@ class Triangle:
         s = (self.size ** 2 * math.sqrt(3)) / 4
         return s
 
+
+class Rectangle:
+    def __init__(self, coord_x=0, coord_y=0, size=100, color="red"):
+        self.coord_x = coord_x
+        self.coord_y = coord_y
+        self.size = size
+        self.color = color
+
+    def move(self, distance_x, distance_y):
+        self.coord_x += distance_x
+        self.coord_y += distance_y
+
+    def area(self):
+        s = 2 * self.size * self.size
+        return s
+
+    def draw(self):
+        turtle.penup()
+        turtle.goto(self.coord_x, self.coord_y)
+        turtle.pendown()
+        turtle.fillcolor(self.color)
+        turtle.begin_fill()
+        for i in range(1, 5):
+            turtle.forward(self.size + self.size * (i % 2))
+            turtle.left(90)
+        turtle.end_fill()
 
